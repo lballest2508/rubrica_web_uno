1d6eed34a45445b67aa2967ebbcbe1bae046dc14
Lballest2508 <lballest@cuc.edu.co>
2023-11-20 20:42:31 -0500

    Agregando informacion estatica para visualizar

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import { BookTable } from './BookTable'; // Crear este componente para la tabla de libros
-import { BookForm } from './BookForm'; // Crear este componente para el formulario de libros
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline, createTheme } from '@mui/material';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import BookIcon from '@mui/icons-material/Book';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import { BookTable } from "./BookTable"; // Crear este componente para la tabla de libros
+import { BookForm } from "./BookForm"; // Crear este componente para el formulario de libros
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import BookIcon from "@mui/icons-material/Book";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const style = {
   position: "absolute",
@@ -48,6 +47,38 @@ export const BookList = () => {
 
   const defaultTheme = createTheme();
 
+  const listaDeLibros = [
+    {
+      id: 1,
+      titulo: 'Cien años de soledad',
+      autor: 'Gabriel García Márquez',
+      descripcion: 'Una novela que narra la historia de la familia Buendía en el pueblo ficticio de Macondo.',
+      disponibilidad: 'Disponible',
+    },
+    {
+      id: 2,
+      titulo: '1984',
+      autor: 'George Orwell',
+      descripcion: 'Una novela distópica que presenta un futuro totalitario y vigilado.',
+      disponibilidad: 'Prestado',
+    },
+    {
+      id: 3,
+      titulo: 'To Kill a Mockingbird',
+      autor: 'Harper Lee',
+      descripcion: 'Una novela que aborda temas de injusticia racial y moral en el sur de Estados Unidos.',
+      disponibilidad: 'Disponible',
+    },
+    {
+      id: 4,
+      titulo: 'El señor de los anillos',
+      autor: 'J.R.R. Tolkien',
+      descripcion: 'Una épica de fantasía que sigue las aventuras de Frodo Bolsón en su misión para destruir el Anillo Único.',
+      disponibilidad: 'Prestado',
+    },
+    // Agrega más libros según sea necesario
+  ];
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
@@ -55,7 +86,12 @@ export const BookList = () => {
       <AppBar position="relative">
         <Toolbar>
           <BookIcon sx={{ mr: 1 }} />
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1, ml: 2 }}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1, ml: 2 }}
+          >
             Biblioteca Unicosta
           </Typography>
           <IconButton color="inherit">
@@ -65,18 +101,23 @@ export const BookList = () => {
       </AppBar>
 
       {/* Lista de libros */}
-      <BookTable />
+      <BookTable books={listaDeLibros} />
 
       {/* Modal para registrar libros */}
-      <Modal aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description" open={isModalOpen} onClose={handleCloseModal} closeAfterTransition
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        closeAfterTransition
         slots={{ backdrop: Backdrop }}
         slotProps={{
           backdrop: {
             timeout: 500,
           },
-        }}>
-        <Fade in={open}>
+        }}
+      >
+        <Fade in={isModalOpen}>
           <Box sx={style}>
             <BookForm handleCloseModal={handleCloseModal} />
           </Box>
@@ -84,9 +125,18 @@ export const BookList = () => {
       </Modal>
 
       {/* Botón de añadir nuevo libro */}
-      <Fab color="primary" aria-label="add" onClick={handleOpenModal}>
-        <AddIcon />
-      </Fab>
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: '20px', // Ajusta según sea necesario
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      >
+        <Fab color="primary" aria-label="add" onClick={handleOpenModal}>
+          <AddIcon />
+        </Fab>
+      </Box>
     </ThemeProvider>
   );
 };
--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -1,12 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Pagination from '@mui/material/Pagination';
+import Button from '@mui/material/Button';
+import { Box, Typography } from '@mui/material';
 
-export const BookTable = () => {
-    // Lógica para obtener la lista de libros (puedes usar un estado o un servicio, dependiendo de tu aplicación)
+export const BookTable = ({ books }) => {
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [filter, setFilter] = useState('');
 
-    return (
+  const filteredBooks = books.filter(
+    (book) =>
+      book.titulo.toLowerCase().includes(filter.toLowerCase()) ||
+      book.autor.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  };
+
+  const handlePrestarClick = (bookId) => {
+    // Lógica para manejar el evento de hacer clic en el botón "prestar"
+    // Puedes implementar la lógica según tus necesidades, como cambiar el estado de disponibilidad del libro, etc.
+    console.log(`Prestar libro con ID: ${bookId}`);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div>
-            {/* Tabla de libros */}
-            {/* Aquí colocarías la lógica para mostrar la lista de libros en una tabla */}
+          <Typography variant="p" component="div" style={{marginLeft: '10px'}}>
+            Lista libros - Mostrando {filteredBooks.length} registros de un total de {books.length}.
+          </Typography>
         </div>
-    );
+        <TextField
+          label="Filtrar por título o autor"
+          variant="filled"
+          margin="normal"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </Box>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Título</TableCell>
+              <TableCell>Autor</TableCell>
+              <TableCell>Descripción</TableCell>
+              <TableCell>Disponibilidad</TableCell>
+              <TableCell>Acciones</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredBooks
+              .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+              .map((book) => (
+                <TableRow key={book.id}>
+                  <TableCell>{book.titulo}</TableCell>
+                  <TableCell>{book.autor}</TableCell>
+                  <TableCell>{book.descripcion}</TableCell>
+                  <TableCell>{book.disponibilidad}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handlePrestarClick(book.id)}
+                      disabled={book.disponibilidad !== 'Disponible'}
+                    >
+                      Prestar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <Pagination
+          count={Math.ceil(filteredBooks.length / rowsPerPage)}
+          page={page}
+          onChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="Libros por página"
+        />
+      </TableContainer>
+    </>
+  );
 };
