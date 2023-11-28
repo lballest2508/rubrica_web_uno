ffc773c214876ab22703c122bb04178f3da3290d
idelahoz14 <idelahoz14@cuc.edu.co>
2023-11-28 10:25:33 -0500

    Protegiendo ruta con acces toen

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, redirect } from 'react-router-dom'
 import { Home } from '../components/Home'
 import { BookList } from '../components/BookList'
 import { Login } from '../components/Login'
 import SignUp from '../components/SingUp'
 import { BookLoans } from '../components/BookLoans'
 
+const token = localStorage.getItem('accessToken');
+
 export const AppRoutes = () => {
-  return (
-    <BrowserRouter>
+  if (token) {
+    return (
+      <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/books" element={<BookList/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/singup" element={<SignUp/>} />
-            <Route path="/loans" element={<BookLoans/>} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/loans" element={<BookLoans />} />
         </Routes>
-    </BrowserRouter>
-  )
+      </BrowserRouter>
+    )
+  } else {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/singup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    )
+  }
 }
